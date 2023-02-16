82da4a32d36445fa065f41dd92352a1a837882c6
HearLing <1402690276@qq.com>
2023-02-16 21:42:58 +0800

    feat: add echarts

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ import router from './router'
 import './assets/main.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as echarts from "echarts";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+app.config.globalProperties.$echarts = echarts
 
 app.mount('#app')
