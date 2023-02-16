7c435e1b5154e1a010b027ecc7c04ed8af57213d
HearLing <1402690276@qq.com>
2023-02-16 21:43:32 +0800

    feat: 增加首页

---
+++ src/components/Card.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="tile">
+    <div class="intro">
+      <div class="title">
+        <img src="https://github.com/HearLing.png" alt="" />
+        <h1>管理员 - HearLing</h1>
+      </div>
+      <div>
+        <h3 style="margin-bottom:10px">TODO List</h3>
+        <el-progress :percentage="65.5" color="#42b983"></el-progress>
+        首页
+        <el-progress :percentage="10.5" color="#f1e05a"></el-progress>
+        代办列表
+        <el-progress :percentage="5.5"></el-progress>
+        其它页面规划
+      </div>
+    </div>
+    <div class="text">
+      <h2 class="animate-text">我的前端学习网站 - <a href="https://chodocs.cn/">ChoDocs</a></h2>
+      <p class="animate-text">
+        一站式前端内容网站，完善前端的学习路线、知识体系
+        项目已经在 GitHub 上开源，欢迎加入我们一起学习，分享前端知识 😸 ~
+      </p>
+      <a href="https://chodocs.cn/">
+        <div class="dots"><span></span> <span></span> <span></span></div>
+      </a>
+    </div>
+</div>
+</template>
+
+<script setup lang="ts">
+
+
+
+
+
+
+
+</script>
+
+<style scoped lang="scss">
+.tile {
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  cursor: pointer;
+  transition: all 0.4s ease-out;
+
+  .intro {
+    width: 100%;
+    position: absolute;
+    transition: all 0.4s ease-out;
+
+    .title {
+      display: flex;
+      align-items: center;
+      margin-bottom: 10px;
+    }
+
+    h1 {
+      margin-left: 10px;
+      font-weight: 300;
+      text-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
+    }
+  }
+
+  .text {
+    z-index: 99;
+    position: absolute;
+    padding: 30px;
+    height: 100%;
+
+    h2 {
+      font-weight: 100;
+      margin-top: 20px;
+      font-style: italic;
+      transform: translateX(200px);
+    }
+
+    p {
+      font-weight: 300;
+      margin-top: 20px;
+      line-height: 25px;
+      transform: translateX(-200px);
+      transition-delay: 0.2s;
+    }
+  }
+}
+
+img {
+  vertical-align: middle;
+  width: 80px;
+  border-radius: 50%;
+}
+
+.animate-text {
+  opacity: 0;
+  transition: all 0.6s ease-in-out;
+}
+
+.tile:hover .intro {
+  opacity: 0.2;
+}
+
+.tile:hover .animate-text {
+  transform: translateX(0);
+  opacity: 1;
+}
+
+.dots {
+  position: absolute;
+  bottom: 20px;
+  right: 30px;
+  margin: 0 auto;
+  width: 30px;
+  height: 30px;
+  color: currentColor;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.dots span {
+  width: 5px;
+  height: 5px;
+  background-color: currentColor;
+  border-radius: 50%;
+  display: block;
+  opacity: 0;
+  transition: transform 0.4s ease-out, opacity 0.5s ease;
+  transform: translateY(30px);
+}
+
+.tile:hover span {
+  opacity: 1;
+  transform: translateY(0px);
+}
+
+.dots span:nth-child(1) {
+  transition-delay: 0.05s;
+}
+
+.dots span:nth-child(2) {
+  transition-delay: 0.1s;
+}
+
+.dots span:nth-child(3) {
+  transition-delay: 0.15s;
+}
+</style>
