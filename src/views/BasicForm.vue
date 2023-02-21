05c6bf505cb8a702698dc15694806478983456f2
HearLing <1402690276@qq.com>
2023-02-21 12:27:13 +0800

    feat: 增加基础表单

---
+++ src/views/BasicForm.vue
@@ -0,0 +1,82 @@
+<!-- 基础表单页面 -->
+<template>
+  <div class="basic-form-page">
+    <h1>基础表单页面</h1>
+    <el-form :model="form" label-width="120px">
+      <el-form-item label="活动名称">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item label="活动地址">
+        <el-select v-model="form.region" placeholder="please select your zone">
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="活动时间">
+        <el-col :span="11">
+          <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%" />
+        </el-col>
+        <el-col :span="2" class="text-center">
+          <span class="text-gray-500">-</span>
+        </el-col>
+        <el-col :span="11">
+          <el-time-picker v-model="form.date2" placeholder="Pick a time" style="width: 100%" />
+        </el-col>
+      </el-form-item>
+      <el-form-item label="即时交付">
+        <el-switch v-model="form.delivery" />
+      </el-form-item>
+      <el-form-item label="活动类型">
+        <el-checkbox-group v-model="form.type">
+          <el-checkbox label="Online activities" name="type" />
+          <el-checkbox label="Promotion activities" name="type" />
+          <el-checkbox label="Offline activities" name="type" />
+          <el-checkbox label="Simple brand exposure" name="type" />
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="资源">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="Sponsor" />
+          <el-radio label="Venue" />
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="活动形式">
+        <el-input v-model="form.desc" type="textarea" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+// do not use same name with ref
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+</script>
+
+<style lang='scss' scoped>
+.basic-form-page {
+  width: 600px;
+
+  h1 {
+    margin: 0px 52px 20px;
+  }
+}
+</style>
