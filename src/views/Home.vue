0e01c1eafc26e3b59ca3d7cf14c49f4a64512e20
HearLing <1402690276@qq.com>
2023-02-22 17:03:39 +0800

    feat: 默认路由 (#32)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,34 @@
 <script setup lang="ts" name="home">
 import {
   Document,
+  List,
   Menu as IconMenu
 } from '@element-plus/icons-vue'
-import { RouterView, } from 'vue-router'
+import { computed } from 'vue';
+import { RouterView, useRoute, } from 'vue-router'
+const route = useRoute();
+const defaultActive = computed(() => {
+  console.log('route', route)
+  return route.path;
+});
 </script>
 
 <template>
   <div class="fillcontain">
     <el-container class="fillcontain">
       <el-aside class="aside">
-        <el-menu default-active="dashboard" style="min-height: 100%;" theme="dark" router>
-          <el-menu-item index="dashboard"><el-icon><icon-menu /></el-icon>首页
+        <el-menu :default-active="defaultActive" style="min-height: 100%;" theme="dark" router>
+          <el-menu-item index="/dashboard"><el-icon><icon-menu /></el-icon>首页
           </el-menu-item>
           <el-sub-menu index="1">
             <template #title>
               <el-icon>
-                <Document />
+                <List />
               </el-icon>
               <span>表格</span>
             </template>
-            <el-menu-item index="table">简单表格</el-menu-item>
-            <el-menu-item index="complexTable">复杂表格</el-menu-item>
+            <el-menu-item index="/table">简单表格</el-menu-item>
+            <el-menu-item index="/complexTable">复杂表格</el-menu-item>
           </el-sub-menu>
           <el-sub-menu index="2">
             <template #title>
@@ -30,8 +37,8 @@ import { RouterView, } from 'vue-router'
               </el-icon>
               <span>表单</span>
             </template>
-            <el-menu-item index="basicForm">基础表单</el-menu-item>
-            <el-menu-item index="stepForm">分步表单</el-menu-item>
+            <el-menu-item index="/basicForm">基础表单</el-menu-item>
+            <el-menu-item index="/stepForm">分步表单</el-menu-item>
           </el-sub-menu>
         </el-menu>
       </el-aside>
